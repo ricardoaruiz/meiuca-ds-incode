aa2a7c684fd5db1a4034654c1129386fa002b58c
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2022-02-25 21:31:22 -0300

    feat: button styling and behaviors

--- design-system-core/src/components/button/index.js
+++ design-system-core/src/components/button/index.js
@@ -1,23 +1,50 @@
-import { LitElement, html, unsafeCSS } from 'lit'
+import { LitElement, html, css, unsafeCSS } from 'lit'
+import { classMap } from 'lit/directives/class-map.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
 
 import style from './style.scss'
 
 export class DSButton extends LitElement {
 
-  static get styles() {
-      return unsafeCSS(style)
-  }
+  static styles = css`
+    ${unsafeCSS(style)}
+  `
 
+  static properties = {
+    loading: { type: Boolean },
+    disabled: { type: Boolean }
+  }
+  
   constructor() {
       super()
+      this.loading = false
+      this.disabled = false
+  }
+
+  _handleClick() {
+    this.dispatchEvent(new CustomEvent('dsClick', {
+      bubbles: true,
+      composed: true
+    }))
   }
 
   render() {
-      return html`
-          <button>
-              <slot></slot>
-          </button>
-      `
+    return html`
+      <button 
+        class="${
+            classMap({
+                'button': true,
+                'button--loading': this.loading
+            })
+        }"
+        ?disabled=${this.disabled}
+        aria-label="${ifDefined(this.loading ? 'Loading' : undefined)}"
+        aria-disabled="${this.disabled}"
+        @click=${this._handleClick}
+      >
+        <slot></slot>
+      </button>
+    `
   }
 }
 
